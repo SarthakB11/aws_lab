783dcec8f4497f162bf5e8150ea7c0f41a8ecd3d
SarthakB11 <100398847+SarthakB11@users.noreply.github.com>
2023-10-14 16:21:05 +0530

    Delete three/third directory

--- three/third/third/side/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index),
-    path("one/", views.one),
-    path("two/", views.two),
-    path("three/", views.three)
-]

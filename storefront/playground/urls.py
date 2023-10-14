98e1a8fee0ea6f827065671ccdd9223d2af1fe03
SarthakB11 <100398847+SarthakB11@users.noreply.github.com>
2023-10-14 16:20:54 +0530

    Delete storefront directory

--- storefront/playground/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("one/", views.one),
-    path("two/", views.two),
-    path("three/", views.three),   
-    path("", views.index)
-]
